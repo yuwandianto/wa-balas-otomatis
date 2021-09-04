ddeb301c02318a91ef939096d2c3de4dd5aa0336
Alvian <45722277+yuwandianto@users.noreply.github.com>
2021-09-04 09:04:47 +0800

    eddit setting puppeteer

---
+++ app.js
@@ -0,0 +1,29 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client({
+    restartOnAuthFail: true,
+    puppeteer: {
+        headless: true,
+        args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu'
+        ],
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
